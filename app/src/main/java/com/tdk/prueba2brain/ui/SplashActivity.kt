261b66e2eabd316536d20b75ca63e32beea25ce5
TDKsKy <jeg33991@gmail.com>
2022-03-08 15:34:09 -0400

    aqui agrege los label y los drop down list

--- app/src/main/java/com/tdk/prueba2brain/ui/SplashActivity.kt
+++ app/src/main/java/com/tdk/prueba2brain/ui/SplashActivity.kt
@@ -1,14 +1,11 @@
 package com.tdk.prueba2brain.ui
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.tdk.prueba2brain.MainActivity
 import com.tdk.prueba2brain.R
-import kotlinx.coroutines.*
-import java.util.concurrent.TimeUnit
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
