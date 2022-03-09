dfbe4f18512fd24603153b83619a2f6897024d56
TDKsKy <jeg33991@gmail.com>
2022-03-08 22:46:22 -0400

    se lo coloca los bordes redondeados a los spinner, se le coloca el fondo blanco a las pantallas

--- app/src/main/java/com/tdk/prueba2brain/MainActivity.kt
+++ app/src/main/java/com/tdk/prueba2brain/MainActivity.kt
@@ -3,8 +3,10 @@ package com.tdk.prueba2brain
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
 import com.tdk.prueba2brain.model.GeneralRatesResponse
 import com.tdk.prueba2brain.network.ApiInterface
+import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -32,6 +34,7 @@ class MainActivity : AppCompatActivity() {
 
                 if(response?.body() != null){
                     Log.i("JOELAPI EXITOSO", response.body().toString())
+                    setUpSpinner1(response.body().rates.keys.toList())
                 }
 
             }
@@ -45,4 +48,9 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    private fun setUpSpinner1(list: List<String>){
+        val adapter = ArrayAdapter(this,
+            android.R.layout.simple_spinner_item, list)
+        spinner_cambiario.adapter = adapter
+    }
 }
