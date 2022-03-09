67ca08412ff18f78cf5381c660a398699ea0d09c
TDKsKy <jeg33991@gmail.com>
2022-03-08 20:53:13 -0400

    aqui se llamo al API y se obtuvo los datos del servidor

--- app/src/main/java/com/tdk/prueba2brain/model/GeneralRatesResponse.kt
+++ app/src/main/java/com/tdk/prueba2brain/model/GeneralRatesResponse.kt
@@ -1,5 +1,12 @@
 package com.tdk.prueba2brain.model
 
-class GeneralRatesResponse {
+import com.google.gson.annotations.SerializedName
 
-}
+data class GeneralRatesResponse(
+    @SerializedName("success")val success: Boolean,
+    @SerializedName("timestamp")val timestamp: Long,
+    @SerializedName("base")val base: String,
+    @SerializedName("date")val date: String,
+    @SerializedName("rates") val rates: Map<String, Double>
+)
+
